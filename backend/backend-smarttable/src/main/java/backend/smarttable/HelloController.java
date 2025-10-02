03afb3ffd2a456b2a6130c2e8c8cf38a7cae1169
Matheus Okano <matheusokano11@gmail.com>
2025-10-01 23:03:38 -0300

    feat: adiciona DynamoDBConfig, entidade Produto anotada e ProdutoRepositorio (Slide 3)

---
+++ backend/backend-smarttable/src/main/java/backend/smarttable/HelloController.java
@@ -0,0 +1,13 @@
+package backend.smarttable;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "🚀 SmartTable Backend está rodando!";
+    }
+}
